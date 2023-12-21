cfaa4e0b4864ddc60b3ed257bd2e655e551d48b6
ec2-user <ec2-user@Mac-mini.local>
2023-12-21 13:25:12 +0000

    2.3.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(name: "LRAtsMediationAdapter",
                         )
                       ],
                       targets: [
-                        .binaryTarget(name: "LRAtsSDK", url: "https://ats-sdk-ios-prod.launch.liveramp.com/2.2.1/LRAtsSDK.zip", checksum: "8ee30f766c227de80329b399ef585d5394a4578e02e784c3f53870f3ceed643e"),
-                        .binaryTarget(name: "LRAtsSDKMediationAdapter", url: "https://ats-sdk-ios-prod.launch.liveramp.com/2.2.1/LRAtsSDKMediationAdapter.zip", checksum: "027739480539d67218939f4f7407022d9c907bc31a7f28a315a3c138bf658fa0"),
+                        .binaryTarget(name: "LRAtsSDK", url: "https://ats-sdk-ios-prod.launch.liveramp.com/2.3.0/LRAtsSDK.zip", checksum: "1aaa30e3abc374cce46b364acbca52a23eb1f0da1d84d85d0606a5bf95323b47"),
+                        .binaryTarget(name: "LRAtsSDKMediationAdapter", url: "https://ats-sdk-ios-prod.launch.liveramp.com/2.3.0/LRAtsSDKMediationAdapter.zip", checksum: "6d909ea5fc306980e47daa89e8fe8af8c45ae6ca731e8cdc261fd66319c626f4"),
                         .target(name: "LRAtsMediationAdapterWrapper",
                                 dependencies: [
                                     "LRAtsSDK",
